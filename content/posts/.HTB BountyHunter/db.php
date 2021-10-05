b73b21ff5ffe32d583d41ae81d8453c0992785eb
OxNinja <57400445+OxNinja@users.noreply.github.com>
2021-10-05 18:57:33 +0200

    .

--- content/posts/.HTB BountyHunter/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// TODO -> Implement login system with the database.
-$dbserver = "localhost";
-$dbname = "bounty";
-$dbusername = "admin";
-$dbpassword = "m19RoAU0hP41A1sTsq6K";
-$testuser = "test";
-?>
